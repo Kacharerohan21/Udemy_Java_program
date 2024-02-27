3fc6d8b6ef7775e9a8aad44437953360e75a3f83
Shubham Kachare <Kacharerohan21@gmail.com>
2024-02-27 17:50:53 +0530

    The Basic Java codes...

---
+++ Add.java
@@ -0,0 +1,10 @@
+package JavaTutorial;
+
+public class Add {
+
+	public static void main(String[] args) {
+	int a=5, b=10;
+	System.out.println("Add :" +(a-b));
+	System.out.println("add :" +(a+b));
+	}
+}
